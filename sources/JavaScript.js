a962034df7906934fdb1a4f358e6a74f0787def2
GustavoSantana <santanagustavi@gmail.com>
2025-04-05 16:07:01 +0900

    first commit

---
+++ sources/JavaScript.js
@@ -0,0 +1,10 @@
+// Smooth scroll to sections when clicking the navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+  
